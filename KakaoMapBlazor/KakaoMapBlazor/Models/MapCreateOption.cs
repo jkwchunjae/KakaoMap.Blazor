70b97df6342ebefc948ddd88f10c0276926a6934
Kyungwon Jeon <jkwchunjae@daum.net>
2022-03-06 12:59:36 +0900

    Create MapCreateOption.cs

---
+++ KakaoMapBlazor/KakaoMapBlazor/Models/MapCreateOption.cs
@@ -0,0 +1,19 @@
+﻿namespace KakaoMapBlazor.Models;
+
+public class MapCreateOption
+{
+    public LatLng Center { get; set; }
+    public int? Level { get; set; }
+    public MapType? MapTypeId { get; set; }
+    public bool? Draggable { get; set; }
+    public bool? Scrollwheel { get; set; }
+    public bool? DisableDoubleClick { get; set; }
+    public bool? DisableDoubleClickZoom { get; set; }
+    public string? ProjectionId { get; set; }
+    public bool? TileAnimation { get; set; }
+
+    public MapCreateOption(LatLng center)
+    {
+        Center = center;
+    }
+}
