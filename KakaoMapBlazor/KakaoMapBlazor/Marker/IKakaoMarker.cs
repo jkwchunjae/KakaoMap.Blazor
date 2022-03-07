51d6e40c998307121ad2ccb6329449792f2ad42b
Kyungwon Jeon <jkwchunjae@daum.net>
2022-03-08 01:07:37 +0900

    Marker methods

--- KakaoMapBlazor/KakaoMapBlazor/Marker/IKakaoMarker.cs
+++ KakaoMapBlazor/KakaoMapBlazor/Marker/IKakaoMarker.cs
@@ -2,6 +2,33 @@
 
 public interface IKakaoMarker
 {
+    ValueTask SetImage(MarkerImage image);
+    ValueTask<MarkerImage> GetImage();
+    ValueTask SetPosition(LatLng position);
+    //ValueTask SetPosition(ViewPoint position);
+    ValueTask<LatLng> GetPosition();
+    //ValueTask SetZIndex(int zIndex);
+    //ValueTask<int> GetZIndex();
+    ValueTask SetVisible(bool visible);
+    ValueTask<bool> GetVisible();
+    ValueTask SetTitle(string title);
+    ValueTask<string> GetTitle();
+    //ValueTask SetDraggable(bool draggable);
+    //ValueTask<bool> GetDraggable();
+    //ValueTask SetClickable(bool clickable);
+    //ValueTask<bool> GetClickable();
+    //ValueTask SetAltitude(double altitude);
+    //ValueTask<double> GetAltitude();
+    //ValueTask SetRange(double range);
+    //ValueTask<double> GetRange();
+    //ValueTask SetOpacity(double opacity);
+    //ValueTask<double> GetOpacity();
+
     event EventHandler Click;
+    //event EventHandler MouseOver;
+    //event EventHandler MouseOut;
+    //event EventHandler RightClick;
+    //event EventHandler DragStart;
+    //event EventHandler DragEnd;
 }
 
---
+++ KakaoMapBlazor/KakaoMapBlazor/Marker/KakaoMarker_Method.cs
@@ -0,0 +1,44 @@
+﻿namespace KakaoMapBlazor.Marker;
+
+public partial class KakaoMarker : IKakaoMarker
+{
+    public async ValueTask SetImage(MarkerImage image)
+    {
+        await _marker!.InvokeVoidAsync("setImage", image);
+    }
+
+    public ValueTask<MarkerImage> GetImage()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SetPosition(LatLng position)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<LatLng> GetPosition()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SetVisible(bool visible)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<bool> GetVisible()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask SetTitle(string title)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<string> GetTitle()
+    {
+        throw new NotImplementedException();
+    }
+}
